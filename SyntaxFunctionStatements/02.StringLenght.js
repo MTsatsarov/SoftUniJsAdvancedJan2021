412ba674a24f4a087c2961ac15c6afcc16c588eb
MTsatsarov <M.Tsatsarov92@gmail.com>
2021-07-02 23:24:32 +0300

    02. stringLenght

---
+++ SyntaxFunctionStatements/02.StringLenght.js
@@ -0,0 +1,6 @@
+function stringLenght (inputOne, inputTwo, inputThree) {
+    let length= inputOne.length+inputTwo.length+inputThree.length;
+    let averageLenght = Math.floor(length/3);
+    console.log(length);
+    console.log(averageLenght);
+}
