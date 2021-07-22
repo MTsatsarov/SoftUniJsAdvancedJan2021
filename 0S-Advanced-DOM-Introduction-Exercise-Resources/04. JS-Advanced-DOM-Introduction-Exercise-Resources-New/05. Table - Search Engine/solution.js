972f95224e981e16ffa14502d294a89e7ebfab72
MTsatsarov <M.Tsatsarov92@gmail.com>
2021-07-22 21:03:41 +0300

    Task 5

--- 0S-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 0S-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,22 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
+ 
    function onClick() {
-      //   TODO:
+      var tbody = [...document.querySelectorAll('tbody tr')];
+      let input = document.querySelector('#searchField').value;
+      for (let i = 0; i < tbody.length; i++) {
+         var element = tbody[i];
+
+         var name = element.querySelector('td').textContent.toLowerCase();
+         if (name.includes(input.toLowerCase())) {
+            element.classList.add('select');
+         }
+         else  {
+            element.classList.remove('select');
+         }
+
+      }
 
    }
 }
+
