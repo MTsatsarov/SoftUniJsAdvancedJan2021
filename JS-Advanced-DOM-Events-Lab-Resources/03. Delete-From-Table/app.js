83318c2bf265726ce9d17623d16530ae691d2dd6
MTsatsarov <M.Tsatsarov92@gmail.com>
2021-07-25 21:40:31 +0300

    Tasks 3 and 5

--- JS-Advanced-DOM-Events-Lab-Resources/03. Delete-From-Table/app.js
+++ JS-Advanced-DOM-Events-Lab-Resources/03. Delete-From-Table/app.js
@@ -1,3 +1,19 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let email = document.querySelector('input[name="email"]').value;
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+
+    let deleted = false;
+    for (let row of rows) {
+        if (row.children[1].textContent == email) {
+            row.parentNode.removeChild(row);
+            deleted = true;
+            document.getElementById('result').textContent='Deleted.';
+        }
+
+    }
+
+    if (!deleted) {
+        document.getElementById('result').textContent='Not Found.';
+    }
+
 }
--- JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
+++ JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+   Array.from(document.querySelectorAll('input')).forEach(i => {
+        i.addEventListener('focus', onFocus);
+        i.addEventListener('blur', onBlur);
+    });
+}
+
+
+function onFocus(ev) {
+    ev.target.parentNode.className = 'focus';
+
+}
+
+function onBlur(ev) {
+    ev.target.parentNode.className = ' ';
+
 }
